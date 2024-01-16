432302942178dfe2e56c048fd7bdf6f2633e6f18
JoravarSinghPunia <140854182+JoravarSinghPunia@users.noreply.github.com>
2024-01-16 10:20:25 +0000

    GET/api/topics updated to reflect comments

--- Errors/errorHandler.js
+++ Errors/errorHandler.js
@@ -3,11 +3,3 @@ const { response } = require("../Endpoints/app");
 module.exports.incorrectPathError = (request, response) => {
   response.status(404).send({ msg: "Not Found" });
 };
-
-module.exports.psqlErrorHandler = (request, response) => {
-  if (err.code === "22P02") {
-    res.status(400).send({ msg: "Bad request" });
-  } else {
-    next(err);
-  }
-};
