d344537c69d8fd265ac46a2a4702ccf68b599052
JoravarSinghPunia <140854182+JoravarSinghPunia@users.noreply.github.com>
2024-01-19 10:03:24 +0000

    updated listen.js

--- listen.js
+++ listen.js
@@ -1,4 +1,4 @@
-const app = require("../be-nc-news/Endpoints/app");
+const app = require("./Endpoints/app");
 const { PORT = 9090 } = process.env;
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
