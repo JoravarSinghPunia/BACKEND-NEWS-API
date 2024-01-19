0919b44f6cde1324c9ae2061e37796aca878619f
JoravarSinghPunia <140854182+JoravarSinghPunia@users.noreply.github.com>
2024-01-19 11:06:16 +0000

    updated listen file for hosting

--- listen.js
+++ listen.js
@@ -1,4 +1,5 @@
 const app = require("./app");
+
 const { PORT = 9090 } = process.env;
 
-app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
+app.listen(9090, () => console.log(`Listening on ${PORT}...`));
